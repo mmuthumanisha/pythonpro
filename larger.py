87980c032f8b5c046ce5eacb8c249282dc5ef7a4
mmuthumanisha <43812099+mmuthumanisha@users.noreply.github.com>
2019-02-25 14:10:26 +0530

    Create larger.py

---
+++ larger.py
@@ -0,0 +1,9 @@
+a=int("enter the integer")
+b=int("enter the integer")
+c=int("enter the integer")
+if(a>b)and(a>c):
+    print("a is larger")
+elseif(b>c)and(b<a):
+    print("b is larger")
+else:
+    print("a is larger")
